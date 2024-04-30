19afa5f0934aa21adf8ebde5f05e3d305a2de053
nishthaaggarwal15 <nishthaaggarwal15@gmail.com>
2024-04-30 21:30:22 +0530

    updated

--- src/practiceset6.java
+++ src/practiceset6.java
@@ -14,26 +14,54 @@ public class practiceset6 {
 
         // WAP to find out wthether the given integer is presenet in array or not
         //Or GUESS THE NUMBER
-         int [] numbers= { 2, 4,5, 6,7};
+//         int [] numbers= { 2, 4,5, 6,7};
+//
+//        Scanner sc= new Scanner(System.in);
+//
+//        System.out.println("Guess the Number");
+//         int num= sc.nextInt();
+//         boolean isInArray= false;
+//
+//         for(int element:numbers){
+//             if(num== element){
+//                 isInArray= true;
+//                 break;
+//             }
+//
+//             }
+//        if(isInArray){
+//            System.out.println("You guessed it right");
+//        }
+//        else {
+//            System.out.println("You guessed it wrong");
+//         }
 
-        Scanner sc= new Scanner(System.in);
 
-        System.out.println("Guess the Number");
-         int num= sc.nextInt();
-         boolean isInArray= false;
+        // Calculate the average marks from an array containing marks of all students in Physics using for each loop
 
-         for(int element:numbers){
-             if(num== element){
-                 isInArray= true;
-                 break;
-             }
+//        int [] marks = { 45,65,89,56,70};
+//        int summ= 0;
+//        for (int elements: marks){
+//            summ = summ + elements;
+//
+//        }
+//        System.out.println(" the average marks are " + summ/marks.length);
 
-             }
-        if(isInArray){
-            System.out.println("You guessed it right");
+        //create a JAVA program to add two matrices of 2x3
+        int [][] matrices;
+
+        matrices= new  int[2][3];
+matrices [0][0]= 23;
+matrices [0] [1] = 45;
+matrices [0] [1] = 44;
+        matrices [1][0]= 23;
+        matrices [1] [1] = 45;
+        matrices [1] [2] = 44;
+        int sum= 0;
+        for ( int elements : matrices){
+            sum = sum + elements;
         }
-        else {
-            System.out.println("You guessed it wrong");
-         }
+
+        System.out.println();
     }
 }
