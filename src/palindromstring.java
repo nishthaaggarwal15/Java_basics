d5e0ad34124b894938bb40c915e32e0807eebce5
Nishtha Aggarwal <145250198+nishthaaggarwal15@users.noreply.github.com>
2024-07-04 22:29:26 +0530

    updated

--- src/palindromstring.java
+++ src/palindromstring.java
@@ -1,3 +1,15 @@
+
+/*
+Given a string, determine if it is a palindrome, considering only alphanumeric characters.
+
+Palindrome
+A palindrome is a word, number, phrase, or other sequences of characters which read the same backwards and forwards.
+Example:
+If the input string happens to be, "malayalam" then as we see that this word can be read the same as forward and backwards, it is said to be a valid palindrome.
+
+The expected output for this example will print, 'true'.
+From that being said, you are required to return a boolean value from the function that has been asked to implement.
+ */
 import java.util.Scanner;
 
 public class palindromstring {
